047ed55c24b67cab60b9199e565545fa90004b86
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-09-30 18:16:46 -0300

    Slider Cert&Badges

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -12,3 +12,41 @@ buttonSkills.addEventListener('click', function() {
 
     buttonSkills.textContent = 'Ler mais'
 });
+
+// Btn Slider -- Certificados & Badges
+
+const controls = document.querySelectorAll('.control');
+let currentItem = 0;
+const items = document.querySelectorAll('.item');
+const maxItems = items.length;
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrowLeft');
+        
+        if(isLeft) {
+            currentItem -= 1;
+        }else {
+            currentItem += 1;
+        }
+
+        if(currentItem >= maxItems) {
+            currentItem = 0;
+
+        }
+
+        if(currentItem < 0) {
+            currentItem = maxItems - 1;
+        }
+
+        items.forEach(item => item.classList.remove('currentItem'));
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        });
+
+        items[currentItem].classList.add('currentItem')
+
+    })
+})
