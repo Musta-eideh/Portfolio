dd7883708636592ace474d4cc77b0636e8c95624
Mustafa Eideh <mustafaeideh100@outlook.com>
2024-01-01 14:08:22 -0300

    Open and close but icon not change

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -50,3 +50,36 @@ controls.forEach(control => {
 
     })
 })
+
+// Menu
+
+const hamburguer = document.getElementById("hamburguer");
+const nav = document.querySelector(".nav");
+
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+// function isNavActive() {
+//     const hamburguerIcon = document.getElementById("hamburguer");
+//     return hamburguerIcon.classList.contains("active");
+// }
+
+// console.log(isNavActive());
+
+// // Função para alterar dinamicamente o ícone
+// function toggleIcon() {
+//     const hamburguerIcon = document.getElementById("hamburguer");
+
+//     if (isNavActive()) {
+//         // Media query ativa, altere para o ícone fa-xmark
+//         hamburguerIcon.classList.remove("fa-bars");
+//         hamburguerIcon.classList.add("fa-xmark");
+//     } else {
+//         // Media query não ativa, reverta para o ícone fa-bars
+//         hamburguerIcon.classList.remove("fa-xmark");
+//         hamburguerIcon.classList.add("fa-bars");
+//     }
+// }
+
+// // Chame a função ao carregar a página e sempre que a tela for redimensionada
+// window.addEventListener("load", toggleIcon);
+// window.addEventListener("resize", toggleIcon);
